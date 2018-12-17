81e759c81e16546e12c6e341d5071942ae094a62
Vattikonda <UI299618@rwe.com>
2018-12-17 16:23:49 +0000

    with material

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,12 +1,35 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatSelectModule } from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {
+  MatInputModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatCardModule,MatButtonModule,MatSelectModule
+} from '@angular/material';
+/* import { MatButtonModule, MatSelectModule } from '@angular/material'; */
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
 
 @NgModule({
-      imports: [MatButtonModule, MatFormFieldModule, MatSelectModule
+      imports: [MatButtonModule, 
+        MatFormFieldModule, 
+        MatSelectModule,
+        MatInputModule,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatTableModule,
+        MatCardModule
     ],
-    exports: [MatButtonModule, MatFormFieldModule, MatSelectModule 
-      ],
+    exports: [MatButtonModule, 
+      MatFormFieldModule, 
+      MatSelectModule,
+      MatInputModule,
+      MatToolbarModule,
+      MatSidenavModule,
+      MatTableModule,
+      MatCardModule
+    ],
   })
 
   export class MaterialModule{
